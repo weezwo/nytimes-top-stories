08549d492154bd7c51f96195782d36445362b900
Luisa Scavo <luisa@Pauls-iMac.local>
2017-06-10 16:08:06 -0400

    update spacing in story spec, implement skeleton CLI class

--- lib/nytimes_top_stories/cli.rb
+++ lib/nytimes_top_stories/cli.rb
@@ -1,3 +1,12 @@
-class CLI
+class NytimesTopStories::CLI
+  def call
 
+  end
+
+  def menu
+  end
+
+  def goodbye
+    puts "Check back later for more headlines!"
+  end
 end
--- spec/02_story_spec.rb
+++ spec/02_story_spec.rb
@@ -4,6 +4,7 @@ describe "Story" do
   let!(:story_hash) {{headline: "Florida Man runs amok", byline: "Chuck Scarsdale", summary: "There he goes again"}}
   let!(:another_story) {{headline: "This is another story", byline: "Jane Jabrony", summary: "this is a story"}}
   let!(:an_array_of_stories) {[story_hash, another_story]}
+  
   it "accepts a hash on initialization and creates a Story object from its keys" do
     story = NytimesTopStories::Story.new(story_hash)
     expect(story.headline).to eq(story_hash[:headline])
