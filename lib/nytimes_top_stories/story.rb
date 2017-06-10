33a9c5f6382578856aa9e2f530a75e12c04904eb
Luisa Scavo <luisa@Pauls-iMac.local>
2017-06-10 15:23:22 -0400

    outfit class Story with initialize, .new_from_array, and class/instance variables

--- lib/nytimes_top_stories/story.rb
+++ lib/nytimes_top_stories/story.rb
@@ -1,3 +1,16 @@
 class NytimesTopStories::Story
+  attr_accessor :headline, :byline, :summary
+  @@all = []
+  initialize(story_hash)
+    @headline = story_hash[:headline]
+    @byline = story_hash[:byline]
+    @summary = story_hash[:summary]
+  end
 
+  def self.new_from_array
+    scrape = Scraper.new
+    scrape.get_top_stories.each do |story|
+
+    end
+  end
 end
